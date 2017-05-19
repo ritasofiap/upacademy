a91e6dc3bf99ce7c037346017d585277c8d6e7e5
ritasofiap <ritasofiapais@gmail.com>
2017-05-19 12:17:47 +0100

    aula13

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@
 
 function LoadBooks() {
 
-var clientID = "813842311915-0f78t2dfpsdab2829o7a0c32if99afee.apps.googleusercontent.com";
+	var clientID = "813842311915-0f78t2dfpsdab2829o7a0c32if99afee.apps.googleusercontent.com";
 
-var APIkey = "AIzaSyC6f596A6x4SmCSiAhZ-BrM9_UZ5czZSGg";
+	var APIkey = "AIzaSyC6f596A6x4SmCSiAhZ-BrM9_UZ5czZSGg";
 
 var ShelfID = "1001";  //bookshelf nova (no url a seguiras_coll)
 
 var UserID = "117214680027185876068";
 
 
-	$.ajax({
+$.ajax({
 	
 		url: "https://www.googleapis.com/books/v1/users/" + UserID + "/bookshelves/" + ShelfID + "/volumes?key=" + APIkey   //virgula pq e'um array
 
@@ -24,45 +24,45 @@ var UserID = "117214680027185876068";
 
 
 			var HTMLtoInsert = `
-				<div class="book col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3" id="book1">
- 					<div>	
- 						<div class="cover">
- 							<a class="imglink" target="_blank" href=""><img class="imgadjust img" src=""></a>	
- 							<h1></h1>
- 						</div>
- 					</div>
- 					<div class="text">				
- 						<div class="booktext">
- 							<p class="description"></p>
- 										
- 							<div class="authorsection">
- 								<span class="author">By </span>
- 							 	<span class="authorsresults"></span>
- 							</div>
- 							
- 							<div class="ratingsection">
- 			 					<span class="glyphicon glyphicon-star-empty"> </span>
- 			 					<span class="averagerating"></span>	
-  							</div>		
+			<div class="book col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3" id="book1">
+			<div>	
+			<div class="cover">
+			<a class="imglink" target="_blank" href=""><img class="imgadjust img" src=""></a>	
+			<h1></h1>
+			</div>
+			</div>
+			<div class="text">				
+			<div class="booktext">
+			<p class="description"></p>
 
- 						</div>
- 						 																			
- 						<div class="links">
- 							<a target="_blank" class="linklivros preview" href="">
- 							<span class="glyphicon glyphicon-book"></span>
- 							 Preview</a>	
+			<div class="authorsection">
+			<span class="author">By </span>
+			<span class="authorsresults"></span>
+			</div>
 
- 							<a target="_blank" class="linklivros googleplay" href="">
- 							<span class="glyphicon glyphicon-shopping-cart"></span>
- 							 Google Play 
- 							<span class="priceresults"></span>
- 								
- 							</a>						
- 											
- 						</div>
+			<div class="ratingsection">
+			<span class="glyphicon glyphicon-star-empty"> </span>
+			<span class="averagerating"></span>	
+			</div>		
 
-					</div>
- 				</div>
+			</div>
+
+			<div class="links">
+			<a target="_blank" class="linklivros preview" href="">
+			<span class="glyphicon glyphicon-book"></span>
+			Preview</a>	
+
+			<a target="_blank" class="linklivros googleplay" href="">
+			<span class="glyphicon glyphicon-shopping-cart"></span>
+			Google Play 
+			<span class="priceresults"></span>
+
+			</a>						
+
+			</div>
+
+			</div>
+			</div>
 			`;
 
 			
@@ -95,8 +95,8 @@ var UserID = "117214680027185876068";
  			//
 
 
-		});
-});
+ 		});
+	});
 
 };
 
@@ -108,6 +108,14 @@ LoadBooks();
 
 //----------------------------------------------------------------//
 
+//-------------RELOAD page onclick menu tinderbook--------------------------//
+
+
+$(".reloadpage").click(function(){
+	window.location.reload();
+});
+
+
 //----------CONJUNTO FUNCOES CLICK LIKE/DISLIKE: TRANSIÇAO BOOKS + ADICIONAR À LISTA FINAL NA LAST PAGE------//
 
 $parent = $(".book.active");
@@ -115,58 +123,57 @@ $next = $parent.next(".book");
 $lastpage = $(".lastpage");
 
 
+// function BookTransitiontoLastPage(){		//quando é o last book (vai para lastpage e desaparecem os butoes)
+// 	$parent.fadeOut(50, function(){
+// 			$parent.removeClass("active");
+// 			$(window).scrollTop(0);
 
-function BookTransitiontoLastPage(){		//quando é o last book (vai para lastpage e desaparecem os butoes)
-	$parent.fadeOut(50, function(){
-			$parent.removeClass("active");
-			$(window).scrollTop(0);
+// 			$lastpage.fadeIn(300, function(){
+// 				$lastpage.addClass("active");
+// 					inAnimation = false;
+// 			});		
 
-			$lastpage.fadeIn(300, function(){
-				$lastpage.addClass("active");
-					inAnimation = false;
-			});		
+// 			$(".buttonsLD").fadeOut(50, function(){
+// 				$(".buttonsLD").removeClass("active");
+// 			});
 
-			$(".buttonsLD").fadeOut(50, function(){
-				$(".buttonsLD").removeClass("active");
-			});
+// 			// $('.recommendations').addClass("active");
+// 			$('.otherrecom').addClass("active");
 
-			// $('.recommendations').addClass("active");
-			$('.otherrecom').addClass("active");
-
-		});
-};
-
-//-----//
-
-function BookTransition(){				//entre books (vai para o next book)
-	$parent.fadeOut(50, function(){					//colocar delay(100)
-		$parent.removeClass("active");
-
-			$(window).scrollTop(0);
-
-		$next.fadeIn(300, function(){
-			$next.addClass("active");
-			inAnimation = false;	
-
-		});
-	});
-};
+// 		});
+// };
 
 //-----//
 
-function AddToLikes(){					//adiciona cover à lista dos likes
-	$cover = $parent.find('.imglink');
-	$cover.clone().appendTo('.listalikes');
-	$('.listalikes').find('.imgadjust').css("max-height","200px").css("margin-top","30px").css("margin-bottom","30px");
-};
+// function BookTransition(){				//entre books (vai para o next book)
+// 	$parent.fadeOut(50, function(){					//colocar delay(100)
+// 		$parent.removeClass("active");
+
+// 			$(window).scrollTop(0);
+
+// 		$next.fadeIn(300, function(){
+// 			$next.addClass("active");
+// 			inAnimation = false;	
+
+// 		});
+// 	});
+// };
 
 //-----//
 
-function AddToDislikes(){				//adiciona cover à lista dos dislikes
-		$cover = $parent.find('.imglink');
-		$cover.clone().appendTo('.listadislikes');
-		$('.listadislikes').find('.imgadjust').css("max-height","200px").css("margin-top","30px").css("margin-bottom","30px");
-};
+// function AddToLikes(){					//adiciona cover à lista dos likes
+// 	$cover = $parent.find('.imglink');
+// 	$cover.clone().appendTo('.listalikes');
+// 	$('.listalikes').find('.imgadjust').css("max-height","200px").css("margin-top","30px").css("margin-bottom","30px");
+// };
+
+// //-----//
+
+// function AddToDislikes(){				//adiciona cover à lista dos dislikes
+// 	$cover = $parent.find('.imglink');
+// 	$cover.clone().appendTo('.listadislikes');
+// 	$('.listadislikes').find('.imgadjust').css("max-height","200px").css("margin-top","30px").css("margin-bottom","30px");
+// };
 
 //-----//
 
@@ -184,28 +191,58 @@ function ClickLike(){				//qdo clicka like (adiciona à lista likes + transita p
 		registolikes.push(true);
 		
 		if(!inAnimation){	
-	
+
 			inAnimation = true;
 			$parent = $(".book.active");
 			$next = $parent.next(".book");
 			$lastpage = $(".lastpage");
-	
-				AddToLikes();
-				ReporCorStar();
+
+			$cover = $parent.find('.imglink');  		 //addtolikes
+			$cover.clone().appendTo('.listalikes');
+			$('.listalikes').find('.imgadjust').css("max-height","200px").css("margin-top","30px").css("margin-bottom","30px");
+
+			$(".likestar.glyphicon-star").css("color","#9999ff");   //repor cor star
 
 			if ($parent.index() == $(".book").length-1){
-		
-				BookTransitiontoLastPage();
-			//inAnimation = false;	
-	
-			}else{
 
-				BookTransition();	
+				$parent.fadeOut(50, function(){			//book transition to last page
+					$parent.removeClass("active");
+					$(window).scrollTop(0);
+
+					$lastpage.fadeIn(300, function(){
+						$lastpage.addClass("active");
+						
+					});		
+
+					$(".buttonsLD").fadeOut(50, function(){
+						$(".buttonsLD").removeClass("active");
+					});
+
+			// $('.recommendations').addClass("active");
+			$('.otherrecom').addClass("active");
+			inAnimation = false;
+		});
+
+
 			//inAnimation = false;	
 
-			};
+		}else{
+
+				$parent.fadeOut(50, function(){					//book transition to next
+					$parent.removeClass("active");
+
+					$(window).scrollTop(0);
+
+					$next.fadeIn(300, function(){
+						$next.addClass("active");
+						inAnimation = false;	
+					});
+				});
+			//inAnimation = false;	
+
 		};
-	});
+	};
+});
 };
 
 ClickLike();
@@ -220,24 +257,55 @@ function ClickDislike(){			//qdo clicka dislike (adiciona à lista dislikes + tr
 		registolikes.push(false);
 
 		if(!inAnimation){	
-	
-			inAnimation = true;
+
+			inAnimation = true;					
 			$parent = $(".book.active");
 			$next = $parent.next(".book");
 			$lastpage = $(".lastpage");
-	
-			ReporCorStar();
-			AddToDislikes();
+
+			$cover = $parent.find('.imglink');							//add to dislikes
+			$cover.clone().appendTo('.listadislikes');
+			$('.listadislikes').find('.imgadjust').css("max-height","200px").css("margin-top","30px").css("margin-bottom","30px");  
+
+			$(".likestar.glyphicon-star").css("color","#9999ff");    //repor cor star
 
 			if ($parent.index() == $(".book").length-1){	//se last book
-		
-				BookTransitiontoLastPage();
-				inAnimation = false;
+
+				$parent.fadeOut(50, function(){					//book transition to last page
+					$parent.removeClass("active");
+					$(window).scrollTop(0);
+
+					$lastpage.fadeIn(300, function(){
+						$lastpage.addClass("active");
+					});		
+
+					$(".buttonsLD").fadeOut(50, function(){
+						$(".buttonsLD").removeClass("active");
+					});
+
+			// $('.recommendations').addClass("active");
+			$('.otherrecom').addClass("active");
+
+			inAnimation = false;
+
+		});
+				
 
 			}else{
 
-				BookTransition();
-				inAnimation = false;
+				$parent.fadeOut(50, function(){					//book transition to next
+					$parent.removeClass("active");
+
+					$(window).scrollTop(0);
+
+					$next.fadeIn(300, function(){
+						$next.addClass("active");
+						inAnimation = false;	
+
+					});
+					inAnimation = false;
+				});
+				
 			};
 		};
 
@@ -253,84 +321,117 @@ function Skip(){				//qdo clicka like (adiciona à lista likes + transita para o
 
 	$("button.skip").on( "click", function() {
 
-		
-		
-		
 		if(!inAnimation){	
-	
+
 			inAnimation = true;
 			$parent = $(".book.active");
 			$next = $parent.next(".book");
 			$lastpage = $(".lastpage");
-	
-				
-				ReporCorStar();
+
+
+			$(".likestar.glyphicon-star").css("color","#9999ff"); //repor cor star
 
 			if ($parent.index() == $(".book").length-1){
-		
-				BookTransitiontoLastPage();
-			//inAnimation = false;	
-	
-			}else{
 
-				BookTransition();	
+				$parent.fadeOut(50, function(){				//book transition to last page
+					$parent.removeClass("active");
+					$(window).scrollTop(0);
+
+					$lastpage.fadeIn(300, function(){
+						$lastpage.addClass("active");
+						inAnimation = false;
+					});		
+
+					$(".buttonsLD").fadeOut(50, function(){
+						$(".buttonsLD").removeClass("active");
+					});
+
+			// $('.recommendations').addClass("active");
+			$('.otherrecom').addClass("active");
+
+		});
 			//inAnimation = false;	
 
-			};
+		}else{
+
+				$parent.fadeOut(50, function(){					//book transition to next
+					$parent.removeClass("active");
+
+					$(window).scrollTop(0);
+
+					$next.fadeIn(300, function(){
+						$next.addClass("active");
+						inAnimation = false;	
+
+					});
+				});
+			//inAnimation = false;	
+
 		};
-	});
+	};
+});
 };
 
 Skip();
 
 
-
-
-
 //-----------------------------------------------------------------------//
-	
+
 //-------------------VOLTAR AO LIVRO ANTERIOR-----------------//
-
-
 
 function ClickBackLike(){			
 
 	$("button.backlike").on( "click", function() {
 
-	$parent = $(".book.active");
-	$previous = $parent.prev(".book");
-	
+		$parent = $(".book.active");
+		$previous = $parent.prev(".book");
+
 		registolikes.pop();
 
 		$(".lista a:last-of-type").remove();
-	
+
 		if ($parent.index() == $(".book").index(0)){	//se first book    experimeentar ao contrario com SE o index for >0
-			$(".backlike").css("color","white");				
+			$(".backlike").css("color","white");
+
 		}else{
-			BookTransitionPrevious();
-			ReporCorStar();
+			
+			$parent = $(".book.active");			//book transition to previous
+			$previous = $parent.prev(".book");
+
+			$parent.fadeOut(50, function(){
+				$parent.removeClass("active");
+
+				$(window).scrollTop(0);
+
+				$previous.fadeIn(300, function(){
+					$previous.addClass("active");
+
+				});
+			});
+
+			$(".likestar.glyphicon-star").css("color","#9999ff");   //repor cor star
 		};
-});
+	});
 };
 
 ClickBackLike();
 
 
-function BookTransitionPrevious(){				//entre books (vai para o prev book)
-	$parent = $(".book.active");
-	$previous = $parent.prev(".book");
+// function BookTransitionPrevious(){				//entre books (vai para o prev book)
+// 	$parent = $(".book.active");
+// 	$previous = $parent.prev(".book");
 
-	$parent.fadeOut(50, function(){
-		$parent.removeClass("active");
+// 	$parent.fadeOut(50, function(){
+// 		$parent.removeClass("active");
 
-			$(window).scrollTop(0);
+// 			$(window).scrollTop(0);
 
-		$previous.fadeIn(300, function(){
-			$previous.addClass("active");
+// 		$previous.fadeIn(300, function(){
+// 			$previous.addClass("active");
 
-		});
-	});
-};
+// 		});
+// 	});
+// };
 
 
 
@@ -344,47 +445,32 @@ $('.dislikecounter').text(counterdislike);
 
 function ContadorLikes() {
 
-$('button.btnlike').click(function() {
+	$('button.btnlike').click(function() {
 		counterlike++;
 		$('.likecounter').text(counterlike);
-		
-		// if (counterlike >=2) {
-		// $('.recommendations').addClass("active");
-		// };
+
 	});
 
 
-$('button.btndislike').click(function() {
+	$('button.btndislike').click(function() {
 		counterdislike++;
 		$('.dislikecounter').text(counterdislike);
-		
-		// if (counterdislike >=2) {
-		// $('.otherrecom').addClass("active");
-		// };
 	});
 
 
 $("button.backlike").click(function() {      //confirmar se está bem
 	
-		if ((islike = true) && (counterlike > 0)){
+	if ((islike = true) && (counterlike > 0)){
 
-			counterlike--;			
-			$('.likecounter').text(counterlike);
+		counterlike--;			
+		$('.likecounter').text(counterlike);
 		
-		}else{			
+	}else if (counterdislike > 0){			
 
-			counterdislike--;
-			$('.dislikecounter').text(counterdislike);
-			};
-
-
+		counterdislike--;
+		$('.dislikecounter').text(counterdislike);
+	};
 });
-
-
-//lenght likes dislikes no fim testar
-// .listalikes > a
-// .listadilikes > a
-// ou $(".lista").children("a").lenght
 
 //TRY AGAIN BUTTON//
 $('.backbtn').click(function() {
@@ -405,9 +491,12 @@ ContadorLikes();
 //-------------------------------------------//
 
 
+
+
 //-------------ANIMAÇÃO SETAS-----------------------------//
 
 function animate(){
+
 	$('.left').fadeTo(25,1).delay(250).fadeTo(25,0.1);
 	$('.middle').delay(250).fadeTo(25,1).delay(250).fadeTo(25,0.1);
 	$('.right').delay(500).fadeTo(25,1).delay(250).fadeTo(25,0.1);
@@ -424,20 +513,20 @@ window.onload = animate(); //para começar logo na pagina inicial e nao ter o de
 
 function TryAgain(){
 
-$(".backbtn").click(function(){
+	$(".backbtn").click(function(){
 
 		$lastpage = $(".lastpage");
 		// $firstbook = $(".book").eq(0);
 		$conjuntobuttons = $(".buttonsLD");
 		$startpage = $(".start");
 
-		ReporCorStar();
-	
+		$(".likestar.glyphicon-star").css("color","#9999ff");
+
 		$lastpage.fadeOut(50, function(){
 			$lastpage.removeClass("active");
 
 			$(window).scrollTop(0);
-	
+
 			$startpage.fadeIn(300, function(){
 				$startpage.addClass("active");
 			});
@@ -447,7 +536,7 @@ $(".backbtn").click(function(){
 			});
 
 		});
-});
+	});
 }
 
 TryAgain();
@@ -458,30 +547,28 @@ TryAgain();
 
 function Start(){
 
-$(".pstart").click(function(){
+	$(".pstart").click(function(){
 
 		$firstbook = $(".book").eq(0);
 		$startpage = $(".start");
 		$conjuntobuttons = $(".buttonsLD");
 
+		$startpage.fadeOut(50, function(){
+			$startpage.removeClass("active");
 
+			$(window).scrollTop(0);
 
-			$startpage.fadeOut(50, function(){
-				$startpage.removeClass("active");
-
-				$(window).scrollTop(0);
-	
-				$firstbook.fadeIn(300, function(){
-					$firstbook.addClass("active");
-				});
+			$firstbook.fadeIn(300, function(){
+				$firstbook.addClass("active");
+			});
 
 		// $(".book").eq(0).addClass("active");
 
-				$conjuntobuttons.fadeIn(0, function(){
-					$conjuntobuttons.addClass("active");
-				});
-			});
-});
+		$conjuntobuttons.fadeIn(0, function(){
+			$conjuntobuttons.addClass("active");
+		});
+	});
+	});
 }
 
 Start();
@@ -494,12 +581,12 @@ function BackFromFavs(){
 	
 
 	$(".backfromfavs").click(function(){
-	
+
 		$(".favorites").fadeOut(50, function(){
 			$(".favorites").removeClass("active");
 
 			$(window).scrollTop(0);
-	
+
 			$firstbook.fadeIn(300, function(){
 				$firstbook.addClass("active");
 			});
@@ -507,15 +594,17 @@ function BackFromFavs(){
 			$conjuntobuttons.fadeIn(0, function(){
 				$conjuntobuttons.addClass("active");
 			});
-
 		});
 
-		});
+		$('.favspage').empty();  //limpa lista dos favs
+	});
 };
 
 
 BackFromFavs();
 
+
+//adicionar botoes para o user remover livros dos favoritos
 
 
 //------------ACEDER AOS FAVORITOS----------//
@@ -531,18 +620,16 @@ function ClickFavs(){
 		$active.fadeOut(50, function(){
 			$active.removeClass("active");
 
-				$(window).scrollTop(0);
+			$(window).scrollTop(0);
 
-				$(".favorites").fadeIn(0, function(){
+			$(".favorites").fadeIn(0, function(){
 				$(".favorites").addClass("active");
-				});
 			});
-});
+		});
+	});
 };
 
 ClickFavs();
-
-
 
 
 //-------------------------------------//
@@ -553,12 +640,12 @@ function AddToFavs(){
 
 	$(".star").click(function(){
 
-	$parent = $(".book.active");
-	$cover = $parent.find('.imglink');
-	$(".likestar.glyphicon-star").css("color","#099FFF");
+		$parent = $(".book.active");
+		$cover = $parent.find('.imglink');
+		$(".likestar.glyphicon-star").css("color","#099FFF");
 
-	$cover.clone().appendTo('.favspage');
-	$('.favspage').find('.imgadjust').css("max-height","200px").css("margin-top","30px").css("margin-bottom","30px").css("display", "inline-block").css("margin","20px");
+		$cover.clone().appendTo('.favspage');
+		$('.favspage').find('.imgadjust').css("max-height","200px").css("margin-top","30px").css("margin-bottom","30px").css("display", "inline-block").css("margin","20px");
 	});
 };
 
@@ -589,13 +676,11 @@ AddToFavs();
 
 // $cover.clone().appendTo('.favspage');
 // $('.favspage').find('.imgadjust').css("max-height","200px").css("margin-top","30px").css("margin-bottom","30px").css("display", "inline-block").css("margin","20px");
-	
+
 // }, function() {
 // $(".favspage").find($cover).remove();
-		
+
 // });
-
-
 
 
 // function RemoveFromFavs(){
@@ -604,7 +689,7 @@ AddToFavs();
 	
 // 		$$parent = $(".book.active");
 // 		$cover = $parent.find('.imglink');
-	
+
 // 		$('.favspage').find($cover).remove();
 
 // 	});
@@ -614,13 +699,12 @@ AddToFavs();
 
 
 
-
 //-----//
 
 
-function ReporCorStar(){				//repor cor inicial da star qdo transita para o next book
-$(".likestar.glyphicon-star").css("color","#9999ff");
-};
+// function ReporCorStar(){				//repor cor inicial da star qdo transita para o next book
+// 	$(".likestar.glyphicon-star").css("color","#9999ff");
+// };
 
 
 //------------------------------------------------------//
@@ -628,12 +712,7 @@ $(".likestar.glyphicon-star").css("color","#9999ff");
 
 
 
-//-------------RELOAD page onclick menu tinderbook--------------------------//
 
-
-$(".reloadpage").click(function(){
-	 window.location.reload();
- });
 
 
 
